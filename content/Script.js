4b14e9dae196f10688a1519c383d163813f89d40
AMESocker <aesocher@yahoo.com>
2022-10-01 20:22:56 -0700

    Psudeo code added to JavaScript

--- content/Script.js
+++ content/Script.js
@@ -1,4 +1,13 @@
+
 //Event listener on the start quiz button
+    //at event action page layout will change by data attributes
+    //Timer will start
+        //Timer will be every one second or 1000 milliseconds
+        //Every second will trigger an set attribute
+        //the changed attribute will be from a decrement function 
 
 //Event listener on each of the answer buttons
-
+    //When any button is clicked a new question is loaded by changing data attributes
+    //If button does not match correct answer then time is reduced by a set amount
+    
+//When Timer reaches zero a finish game screen will appear
