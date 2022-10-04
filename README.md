b17fa0eb2d44a78423a7f949492e2dc11eeaa81a
AMESocker <aesocher@yahoo.com>
2022-10-03 22:27:46 -0700

    Final commit before deployment

--- README.md
+++ README.md
@@ -18,22 +18,26 @@ SO THAT I can gauge my progress compared to my peers
 ## Acceptance Criteria
 GIVEN I am taking a code quiz
 
-A.complete
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
 
-B.complete
-WHEN I answer a question
-THEN I am presented with another question
+    WHEN I click the start button
+    THEN a timer starts and I am presented with a question
 
-C.TODO:
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
+    WHEN I answer a question
+    THEN I am presented with another question
 
-D.TODO:
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
+    WHEN I answer a question incorrectly
+    THEN time is subtracted from the clock
 
-E.TODO:
-WHEN the game is over
-THEN I can save my initials and score
+    WHEN all questions are answered or the timer reaches 0
+    THEN the game is over
+
+    WHEN the game is over
+    THEN I can save my initials and score
+
+## Challenges
+
+    I had trouble getting the syntax of getElementsByClass selector to work. So, in replacement I wrote each element out the needed to be changed.
+
+    I also struggled with the count down timer. At the time of posting (10/3/2022) I still have not a full grasp of the tool and the syntax. The addition of a time out function and a subtraction of time I was not able to de-bug in the allocated time frame.
+
+    Much of the time was used to develop the click-through function of the page. I was not able to put on local storage option to save scores.
--- content/Script.js
+++ content/Script.js
@@ -8,32 +8,39 @@ function startButton(){
     //selecting element
     // document.getElementById("startBox"); 
     //set attribute
+    
+    clock.setAttribute("style", "display: contents");
     startBox.setAttribute("style", "display: none");
     question1Box.setAttribute("style", "display: contents");
-    
+    timer();
+    // setTimeout(() ={
+    //     endBox.setAttribute("style","display:contents");
+    // },6000);
     
 };
 var seconds = 60;
-var clockSelector = document.getElementById('Clock');
+var clockSelector = document.getElementById('clock');
 function counterSeconds(){
     seconds -= 1;
     clockSelector.innerHTML = seconds;
-}
-var cancel = setInterval(counterSeconds,1000);
+};
+function timer(){setInterval(counterSeconds,1000);};
+
 // document.getElementsByClassName("answer").addEventListener("click",nextQuestion);
 //Console doesn't like preceding statement
-
+// var minusTime = ('clock ' -= 15);
 //first question
 document.getElementById('1a').addEventListener("click",nextQuestion);
 document.getElementById('1b').addEventListener("click",nextQuestion);
 document.getElementById('1c').addEventListener("click",correctQuestion);
 document.getElementById('1d').addEventListener("click",nextQuestion);
-document.getElementById('Clock');
+document.getElementById('Clock').minusTime;
 
 function nextQuestion(){ 
     console.log("Put...the candle...back!");
     question1Box.setAttribute("style","display: none");
     question2Box.setAttribute("style","display:contents");
+    document.getElementById('Clock');
 };
 function correctQuestion(){
     console.log("It is pronounced Fronkensteen!");
